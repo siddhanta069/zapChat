d93a3453a79a9c49993c6b20da043d681727e53f
siddhanta sahoo <siddhanta027z@gmail.com>
2026-01-28 21:04:52 +0530

    initial commit

---
+++ backend/src/routes/auth.route.ts
@@ -0,0 +1,17 @@
+import express  from "express";
+
+const router = express.Router();
+
+router.get('/signup', (req, res) => {
+
+})
+
+router.get('/signin', (req, res) => {
+  
+})
+
+router.get('/logout', (req, res) => {
+  
+})
+
+export default router;
---
+++ backend/src/routes/message.route.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/send', (req, res) => {
+
+})
+
+export default router;
